3af8380ce19c878049a794c1ecd5ac715ee2da96
Orbet Lozada <orbetjose@gmail.com>
2026-03-16 15:42:19 -0500

    Ajuste url de imagenes

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "admin.laculpaesdelinternet.com",
+        hostname: "admin.adempiregroup.com",
         port: "",
         pathname: "/**",
       },
